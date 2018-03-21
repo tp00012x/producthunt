95a57321f86d0c649663240ea3de076e28444c6a
Anthony Torres <atorres@g2crowd.com>
2018-03-21 01:31:42 -0500

    finish media

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from products import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
